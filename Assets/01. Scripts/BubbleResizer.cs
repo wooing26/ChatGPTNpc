3b8ce05fe176cc21cb2a5a6c5c38bd7b5768867f
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-06-24 17:35:27 +0900

    카톡 형태 UI로 변경

---
+++ Assets/01. Scripts/BubbleResizer.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class BubbleResizer : MonoBehaviour
+{
+    public TextMeshProUGUI MsgText;
+    public RectTransform   BubbleRt;
+    public float           MaxWidth     = 1500f; // 최대 말풍선 너비
+    public float           WidthOffset  = 100f;
+    public float           HeightOffset = 20f;
+
+    void LateUpdate()
+    {
+        float txtWidth = MsgText.preferredWidth;
+        float width = Mathf.Min(txtWidth, MaxWidth);
+        
+        MsgText.rectTransform.SetSizeWithCurrentAnchors(
+            RectTransform.Axis.Horizontal, width);
+        MsgText.textWrappingMode = TextWrappingModes.Normal;
+
+        float txtHeight = MsgText.preferredHeight;
+
+        BubbleRt.sizeDelta = new Vector2(width + WidthOffset, txtHeight + HeightOffset);
+    }
+}
